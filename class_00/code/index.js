2b2b444dc2b87b4cb09307f28edb923c032db86b
Jack Jeffress <jack.jeffress@ga.co>
2021-08-09 20:18:29 +1000

    Added code from today

---
+++ class_00/code/index.js
@@ -0,0 +1,90 @@
+// console.log(10 + 32);
+
+// console.log(17 - 4);
+
+// // Multiplication
+// console.log(5 * 29);
+
+// // Division
+// console.log(82 / 2);
+
+// // To comment out lines:
+// // CMND + / for Mac
+// // CTRL + / for Windows
+
+// // Booleans
+// console.log(true);
+// console.log(false);
+
+// // Strings
+// console.log("I am double quoted");
+// console.log("I am single quoted");
+
+// // Escaping Quotes
+// console.log("I'm fine");
+// console.log("He said 'okay'");
+
+// console.log('She said "bagus"');
+// // Backslash says ignore the special meaning of the quote here
+
+// // Concatenation - Combining into strings
+// console.log("Hello " + "world");
+// console.log("2 + 2 = " + 4);
+
+// // Properties are static information about the data
+// console.log("hello".length);
+
+// // Methods are operations you can perform on the data
+// // Notice the round brackets - that means run some code!
+// console.log("hello".toUpperCase());
+
+// // undefined - This is empty (it hasn't been defined yet)
+// console.log(undefined);
+
+// // null - This is also empty (it definitely has no value)
+// console.log(null);
+
+// // Structural Data Types
+
+// // Array
+// console.log([]);
+
+// console.log([1, 2, 3]);
+
+// // Object
+// console.log({});
+
+// console.log({ courseName: "JavaScript" });
+
+// // Functions
+// function logHello() {
+//   console.log("Hello");
+// }
+
+// logHello(); // Callsite - run the logHello function!
+// logHello();
+
+let myFirstVar;
+
+console.log(myFirstVar);
+
+let animal = "Orca";
+console.log(animal);
+console.log(animal.length);
+
+let alphabet = ["a", "b", "c"];
+console.log(alphabet);
+
+let myVar = 42;
+myVar = "A string";
+myVar = true;
+myVar = null;
+
+console.log(myVar);
+
+const favouriteNumber = 42;
+
+const myString = "Hello";
+
+console.log(typeof myString);
+console.log(favouriteNumber, myString);
